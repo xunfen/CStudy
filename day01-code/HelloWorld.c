b2e150d6980a7dfafc938dd97e51b81b71456e1b
xunfen <107894831+xunfen@users.noreply.github.com>
2025-09-22 22:58:38 +0800

    Add files via upload

---
+++ day01-code/HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>//预处理，stdio全名Standard Input Output,即stdio.h是头文件
+int main()//程序的主入口，main名字不能改
+{
+    printf("Hello World");//业务代码
+    return 0;//程序运行结束，0代表正常结束，非零则为非正常结束,并且0要与int main()中的int相呼应
+}
