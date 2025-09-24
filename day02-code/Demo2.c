73f00b66d613267f4625fa6a11e96d244b032dcf
xunfen <107894831+xunfen@users.noreply.github.com>
2025-09-24 13:47:51 +0800

    Add files via upload

---
+++ day02-code/Demo2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("a=%d\n", a);
+    a = 20;
+    printf("a=%d\n", a);
+    return 0;
+}
---
+++ day02-code/Test3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 20;
+    printf("a+b=%d\n", a + b);
+    return 0;
+}
