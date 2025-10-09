80de7c9bf23ddd6f224b0e38c30ee350da05c6a3
xunfen <107894831+xunfen@users.noreply.github.com>
2025-10-09 14:01:25 +0800

    Add files via upload

---
+++ day04-code/Demo1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("%d\n", 3 + 2);
+    printf("%d\n", 3 - 2);
+    printf("%d\n", 3 * 2);
+
+    printf("%lf\n", 2.1 + 2);
+    printf("%.2lf\n", 2.1 + 2);
+
+    int a = 3;
+    int b = 2;
+    printf("%d\n", a + b);
+
+
+
+    return 0;
+}
