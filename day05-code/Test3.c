fe065afa41ea539e2b67881ec7820556c0cda5e8
xunfen <107894831+xunfen@users.noreply.github.com>
2025-10-13 21:56:57 +0800

    Add files via upload

---
+++ day05-code/Test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int love = 18;
+    if (love >= 60) {
+        printf("I love you\n");
+    }
+    else {
+        printf("I miss you\n");
+    }
+    return 0;
+
+}
