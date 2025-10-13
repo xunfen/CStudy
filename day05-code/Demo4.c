1facfee7f8b56bb5ffe4420bb5e60a40059a7710
xunfen <107894831+xunfen@users.noreply.github.com>
2025-10-13 21:54:44 +0800

    Add files via upload

---
+++ day05-code/Demo4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    if (a > 10) {
+        printf("a is greater than 10");
+    }
+    else {
+        printf("a is less than 10");
+    }
+    return 0;
+}
