ac99abbd985d387a76a96d75c1a67560ba205c72
xunfen <107894831+xunfen@users.noreply.github.com>
2025-10-21 22:22:46 +0800

    Add files via upload

---
+++ day06-code/Demo4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    /* printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*\n");
+
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*\n");
+
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*\n"); */
+
+    for (int i = 0; i <= 3 ; i++){
+        for (int j = 0; j <= 4; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
